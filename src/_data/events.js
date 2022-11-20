3a904d891f646f9c27f63392396ac9f0a081cb6e
James Doc <james@globe.church>
2022-11-20 11:04:40 +0000

    Swap out iCal parser
    Resolves an issue where recurring events were often off by one hour thanks to daylight savings

--- src/_data/events.js
+++ src/_data/events.js
@@ -8,12 +8,23 @@ if (!process.env.EVENTS_ICAL_FEED) {
 const { AssetCache } = require("@11ty/eleventy-cache-assets");
 const slugify = require("slugify");
 const fetch = require("node-fetch");
-const ical = require('ical');
+const ical = require('node-ical');
 const frontMatter = require('front-matter');
 const striptags = require('striptags');
 
-// TODO: Switch this for DayJS
-const moment = require('moment');
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const duration = require('dayjs/plugin/duration');
+const relativeTime = require('dayjs/plugin/relativeTime');
+const localTimezone = "Europe/London";
+
+dayjs
+  .extend(utc)
+  .extend(timezone)
+  .extend(duration)
+  .extend(relativeTime)
+  .tz.setDefault(localTimezone);
 
 const ENABLE_11TY_CACHE = process.env.ENABLE_11TY_CACHE.toLowerCase() === 'true';
 const icalFeedUrl = process.env.EVENTS_ICAL_FEED;
@@ -47,124 +58,59 @@ module.exports = () => {
 }
 
 function parseEvents(icalRaw) {
-  const rangeStart = moment("2020-01-01");
-  const rangeEnd = moment().add(3, 'months');;
+  const rangeStart = dayjs().subtract(3, 'months');
+  const rangeEnd = dayjs().add(4, 'months');
 
   const events = ical.parseICS(icalRaw);
   const returnEvents = [];
 
   for (let k in events) {
+    if (!Object.prototype.hasOwnProperty.call(events, k)) continue;
 
-    if (events.hasOwnProperty(k)) {
-      var ev = events[k];
+    const event = events[k];
+    if (event.type !== 'VEVENT') continue;
 
-      if (events[k].type == 'VEVENT') {
+    if (!event.rrule) {
+      returnEvents.push(eventBuilder(event));
+    } else {
 
-        let startDate = moment(ev.start);
-        let endDate = moment(ev.end);
+      const dates = event.rrule.between(rangeStart.toDate(), rangeEnd.toDate());
+      if (dates.length === 0) continue;
 
-        const duration = parseInt(endDate.format("x")) - parseInt(startDate.format("x"));
+      // Loop through each date in the recurrence
+      dates.forEach(date => {
 
-        let eventDesc = removeHtmlForParsing(ev.description);
-        eventDesc = frontMatter(eventDesc);
+        let curEvent = event;
+        let dateLookupKey = date.toISOString().substring(0, 10);
+        let showRecurrence = true;
 
-        const slug = slugify(ev.summary, {
-          lower: true,
-          strict: true,
-        })
+        const startDate = dayjs(date).format('YYYY-MM-DD');
+        const startTime = dayjs(curEvent.start).format('HH:mm:ss');
+        let start = dayjs(`${startDate} ${startTime}`);
 
-        // One off events…
-        if (typeof ev.rrule === 'undefined') {
+        let duration = dayjs(event.end).valueOf() - curEvent.start.valueOf();
+        let end = start.add(duration, 'milliseconds');
 
-          returnEvents.push({
-            title: ev.summary,
-            slug: slug,
-            location: ev.location,
-            startDate: startDate.format(),
-            endDate: endDate.format(),
-            duration: moment.duration(duration).humanize(),
-            body: eventDesc.body,
-            data: eventDesc.attributes,
-            reoccurring: false
-          });
+        // Check to see if there is a reoccurance override
+        if ((curEvent.recurrences != undefined) && (curEvent.recurrences[dateLookupKey] != undefined)) {
+          curEvent = curEvent.recurrences[dateLookupKey];
+          start = dayjs(curEvent.start);
+          duration = dayjs(curEvent.end).valueOf() - dayjs(curEvent.start).valueOf();
 
-        } else if (typeof ev.rrule !== 'undefined') {
-
-          const dates = ev.rrule.between(
-            rangeStart.toDate(),
-            rangeEnd.toDate()
-          );
-
-          if (ev.recurrences !== undefined){
-            for (let r in ev.recurrences) {
-              if (moment(new Date(r)).isBetween(rangeStart, rangeEnd) != true) {
-                dates.push(new Date(r));
-              }
-            }
-          }
-
-          for(let i in dates) {
-            var date = dates[i];
-            var curEvent = ev;
-            var showRecurrence = true;
-            var curDuration = duration;
-
-            startDate = moment(date);
-
-            // Use just the date of the recurrence to look up overrides and exceptions (i.e. chop off time information)
-            var dateLookupKey = date.toISOString().substring(0, 10);
-
-            // For each date that we're checking, it's possible that there is a recurrence override for that one day.
-            if ((curEvent.recurrences != undefined) && (curEvent.recurrences[dateLookupKey] != undefined)) {
-              // We found an override, so for this recurrence, use a potentially different title, start date, and duration.
-              curEvent = curEvent.recurrences[dateLookupKey];
-              startDate = moment(curEvent.start);
-              curDuration = parseInt(moment(curEvent.end).format("x")) - parseInt(startDate.format("x"));
-
-            // If there's no recurrence override, check for an exception date.  Exception dates represent exceptions to the rule.
-            } else if ((curEvent.exdate != undefined) && (curEvent.exdate[dateLookupKey] != undefined)) {
-              // This date is an exception date, which means we should skip it in the recurrence pattern.
-              showRecurrence = false;
-            }
-
-            endDate = moment(parseInt(startDate.format("x")) + curDuration, 'x');
-
-            // If this recurrence ends before the start of the date range, or starts after the end of the date range,
-            // don't process it.
-            if (endDate.isBefore(rangeStart) || startDate.isAfter(rangeEnd)) {
-              showRecurrence = false;
-            }
-
-            if (showRecurrence === true) {
-
-              let eventDesc = removeHtmlForParsing(curEvent.description);
-              eventDesc = frontMatter(eventDesc);
-
-              const slug = slugify(curEvent.summary, {
-                lower: true,
-                strict: true,
-              });
-
-              returnEvents.push({
-                title: curEvent.summary,
-                slug: slug,
-                location: curEvent.location,
-                startDate: startDate.format(),
-                endDate: endDate.format(),
-                duration: moment.duration(curDuration).humanize(),
-                body: eventDesc.body,
-                data: eventDesc.attributes,
-                reoccurring: true
-              });
-            }
-
-          }
-
+        // Sometime dates are excluded
+        } else if ((curEvent.exdate != undefined) && (curEvent.exdate[dateLookupKey] != undefined)) {
+          showRecurrence = false;
         }
 
-      }
-    }
+        if (showRecurrence) {
+          curEvent.start = start.toDate();
+          curEvent.end = end.toDate();
 
+          returnEvents.push(eventBuilder(curEvent, true));
+        }
+
+      });
+    }
   }
 
   return returnEvents;
@@ -179,4 +125,31 @@ function removeHtmlForParsing(input) {
 
   let eventDesc = striptags(input, ['br']);
   return eventDesc.replaceAll('<br>','\r\n');
+}
+
+function eventBuilder(event, recurring = false) {
+  start = dayjs(event.start);
+  end = dayjs(event.end);
+
+  let eventDesc = removeHtmlForParsing(event.description);
+  eventDesc = frontMatter(eventDesc);
+
+  const slug = slugify(event.summary, {
+    lower: true,
+    strict: true,
+  });
+
+  const duration = dayjs.duration(end.diff(start)).humanize();
+
+  return {
+    title: event.summary,
+    slug,
+    location: event.location,
+    startDate: start.format(),
+    endDate: end.format(),
+    duration,
+    body: eventDesc.body,
+    data: eventDesc.attributes,
+    recurring
+  }
 }
