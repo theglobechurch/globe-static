014cf296e031b7bad711d6713500d1cb44d04572
James Doc <james@globe.church>
2022-12-24 09:34:13 +0000

    Basic contact details

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,5 +1,12 @@
 module.exports = function () {
+  // TODO: Move this all out to Wordpress
   return {
+    instagram: "theglobechurch",
+    twitter: "theglobechurch",
+    facebook: "theglobechurch",
+    youtube: "theglobechurch",
+    linkedin: "theglobechurch",
+    email: "info@globe.church",
     registeredAddress: "GlobeOffice, 83 Mill Lofts, County St, London SE1 4AD"
   }
 }
