cd0a83f69e507a21d74ee12b7488b8287434749f
James Doc <james@globe.church>
2023-01-21 18:33:03 +0000

    Improve join us block

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -3,7 +3,7 @@ module.exports = function () {
   return {
     title: "The Globe Church",
     subtitle: "All about Jesus. Made up of all sorts of people. Involved in the greatest mission…",
-    sundays: "The Globe Church meets each Sunday, 4pm at Ark Globe Academy, Harper Road, London, SE1 6AF. It's just a few minutes walk from Elephant and Castle Underground station.",
+    sundays: "The Globe Church meets each Sunday, <strong>4pm</strong> at <strong>Ark Globe Academy, Harper Road, London, SE1 6AF</strong>.<br /><br />It's just a few minutes walk from Elephant and Castle Underground station.",
     feed: {
       url: "https://www.globe.church/blog/feed.xml"
     },
