cc288b3e821f5b88d819a0ae0de63f6fdae66145
James Doc <james@globe.church>
2023-02-13 21:38:01 +0000

    Tweak blog post rss feed

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,12 +2,14 @@ module.exports = function () {
   // TODO: Move this all out to Wordpress
   return {
     title: "The Globe Church",
-    subtitle: "All about Jesus. Made up of all sorts of people. Involved in the greatest mission…",
-    sundays: "The Globe Church meets each Sunday, <strong>4pm</strong> at <strong>Ark Globe Academy, Harper Road, London, SE1 6AF</strong>.<br /><br />It's just a few minutes walk from Elephant and Castle Underground station.",
+    subtitle:
+      "All about Jesus. Made up of all sorts of people. Involved in the greatest mission…",
+    sundays:
+      "The Globe Church meets each Sunday, <strong>4pm</strong> at <strong>Ark Globe Academy, Harper Road, London, SE1 6AF</strong>.<br /><br />It's just a few minutes walk from Elephant and Castle Underground station.",
     feed: {
-      url: "https://www.globe.church/blog/feed.xml"
+      url: "https://www.globe.church/blog/feed.xml",
     },
-    url: "https://globe.church",
+    url: "https://www.globe.church",
 
     instagram: "theglobechurch",
     twitter: "theglobechurch",
@@ -15,6 +17,6 @@ module.exports = function () {
     youtube: "theglobechurch",
     linkedin: "theglobechurch",
     email: "info@globe.church",
-    registeredAddress: "GlobeOffice, 83 Mill Lofts, County St, London SE1 4AD"
-  }
-}
+    registeredAddress: "GlobeOffice, 83 Mill Lofts, County St, London SE1 4AD",
+  };
+};
