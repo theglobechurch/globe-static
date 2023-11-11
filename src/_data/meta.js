92b7f1a74daa1a712dfa282e43bcfe21fc858e9d
James Doc <james@globe.church>
2023-11-11 11:10:07 +0000

    feat: pull metadata from wp

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,27 +1,65 @@
-module.exports = function () {
-  // TODO: Move this all out to Wordpress
-  return {
+require("dotenv").config();
+const EleventyFetch = require("@11ty/eleventy-fetch");
+const WP_CACHE_LENGTH = process.env.WP_CACHE_LENGTH || "1d";
+const additionalsUrl = `${process.env.API_BASE.replace('wp/v2/', 'globe/v1/')}additionals`;
+
+const defaultLocation = "Ark Globe Academy, Harper Road, London, SE1 6AF";
+const defaultTime = "4pm";
+const defaultTube = "Elephant and Castle";
+
+module.exports = async () => {
+
+  console.log("[ 🐙 ] Fetching meta data");
+
+  const additionals = await EleventyFetch(additionalsUrl, {
+    duration: WP_CACHE_LENGTH,
+    type: "json"
+  });
+
+  const meta = {
     title: "The Globe Church",
     subtitle:
       "All about Jesus. Made up of all sorts of people. Involved in the greatest mission…",
-    sundays:
-      "The Globe Church meets each Sunday, <strong>4pm</strong> at <strong>Ark Globe Academy, Harper Road, London, SE1 6AF</strong>.<br /><br />It's just a few minutes walk from Elephant and Castle Underground station.",
+    sundays: sundaysLine(additionals['overrides'] || {}),
     feed: {
       url: "https://www.globe.church/blog/feed.xml",
     },
     url: "https://www.globe.church",
 
-    instagram: "theglobechurch",
-    twitter: "theglobechurch",
+    instagram: additionals['instagram'] || "theglobechurch",
+    twitter: additionals['twitter'] || "theglobechurch",
     facebook: "theglobechurch",
     youtube: "theglobechurch",
     linkedin: "theglobechurch",
     email: "info@globe.church",
-    registeredAddress: "GlobeOffice, 83 Mill Lofts, County St, London SE1 4AD",
-    overrides: {
-      serviceTime: "4pm",
-      location: "Haberdashers' Borough Academy, 94b Southwark Bridge Rd, London SE1 0EX",
-      nearestUnderground: "Borough"
-    }
+    registeredAddress: additionals['registeredAddress'] || "GlobeOffice, 83 Mill Lofts, County St, London SE1 4AD",
+    overrides: additionals['overrides'] || null,
+    service: serviceDetails(additionals['overrides'] || {})
   };
+
+  meta.service.locationName = meta.service.fullAddress.split(',')[0];
+
+  return meta
 };
+
+function sundaysLine(overrides) {
+  return `
+    The Globe Church ${overrides ? 'is meeting this' : 'meets each'} Sunday,
+    <strong>${overrides['serviceTime'] || defaultTime}</strong> at
+    <strong>${overrides['location'] || defaultLocation}</strong>.<br /><br />
+    It's just a few minutes walk from ${overrides['nearestUnderground'] || defaultTube}
+    station.`;
+}
+
+function serviceDetails(overrides) {
+  const details = {
+    overrides: !!Object.keys(overrides).length,
+    fullAddress: overrides['location'] || defaultLocation,
+    time: overrides['serviceTime'] || defaultTime,
+    underground: overrides['nearestUnderground'] || defaultTube
+  }
+
+  details.locationName = details.fullAddress.split(',')[0];
+
+  return details;
+}
