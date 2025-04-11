eb13ed8042c24ee02dae60c7103cdf45499ecdb9
James Doc <james@globe.church>
2025-04-11 18:31:52 +0100

    Homepage

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -1,6 +1,9 @@
 import Alpine from 'alpinejs';
 import coreNav from './alpine/navigation.js';
+import { bannerImageCrossFade } from './bannerImage.js';
 
 window.Alpine = Alpine;
 Alpine.data('coreNav', coreNav);
 Alpine.start();
+
+bannerImageCrossFade();
