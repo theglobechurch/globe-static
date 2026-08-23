11e5e4e85243c1a0ae22af477fca3093640712fa
James Doc <james@globe.church>
2026-08-23 14:52:45 +0100

    Update the navigation

--- src/_assets/js/alpine/navigation.js
+++ src/_assets/js/alpine/navigation.js
@@ -4,20 +4,29 @@ export default () => ({
 
   init() {
     this.$watch('navOpen', () => {
-      this.menuLabel = this.navOpen ? 'Close' : 'Menu'
+      this.menuLabel = this.navOpen ? 'Close' : 'Menu';
     });
 
     document.addEventListener('keyup', (event) => this.escapeClose(event));
   },
 
   toggle() {
-    this.navOpen = !this.navOpen
+    this.navOpen = !this.navOpen;
+    // prevent scrolling when the menu is open
+    document.body.classList.toggle('preventScroll', this.navOpen);
+  },
+
+  close() {
+    if (!this.navOpen) return;
+
+    this.navOpen = false;
+    document.body.classList.remove('preventScroll');
   },
 
   // using alpine js listen for the escape key to close the menu
   escapeClose(event) {
     if (event.key === 'Escape' && this.navOpen) {
-      this.navOpen = false
+      this.close();
     }
   }
 })
